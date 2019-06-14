1251b19b90041f5c8c34495360dec2454778a2d7
madhavmansuriya <f201506100110128@gmail.com>
2019-06-14 22:25:48 +0530

    icon update

--- FlyeTask/resources/views/Frame/frame.blade.php
+++ FlyeTask/resources/views/Frame/frame.blade.php
@@ -5,6 +5,19 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="shortcut icon" href="/image/bank.png" type="image/x-icon"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="icon" type="image/x-icon" href="https://www.herokucdn.com/favicons/favicon.ico" />
+    <link rel="mask-icon" href="https://www.herokucdn.com/favicons/icon.svg" color="#79589f">
+    <link rel="apple-touch-icon" type="image/png" href="/image/bank.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="152x152" href="/image/bank.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="167x167" href="/image/bank.png">
+    <link rel="apple-touch-icon" type="image/png" sizes="180x180" href="/image/bank.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/image/bank.png">
+
+
     <title>@yield('title')</title>
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
